4a697c99185a3945be69073b77ac47f7b2c1da62
zerox555 <115931793+zerox555@users.noreply.github.com>
2023-04-18 19:51:02 +0800

    Update main.dart
    Updated main.dart to display the correct loading page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hilti_xxx/pages/LoadingPage.dart';
-import 'package:hilti_xxx/pages/chatbotPage.dart';
 
 void main() {
   runApp(const HiltiComp());
@@ -14,7 +13,7 @@ class HiltiComp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: chatbotPage(),
+      home: LoadingPage(),
     );
   }
 }
